b6bbf12df3730c9c2e32a59373bf13f752db5159
Tigana Sari <tiganasari@Portas-MB15.broadband>
2021-11-01 19:24:16 +0000

    Installed react router, set up components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React, { useEffect, useState} from "react";
 import './App.css';
+import Dashboard from './components/Dashboard'
+import { BrowserRouter as Router, Switch, Route, Link, useHistory } from 'react-router-dom';
 
 function App() {
   return (
+
+    <Router> 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> pet passport </h1>
+     <Dashboard />
     </div>
+
+    </Router> 
   );
 }
 
---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Dashboard = () => {
+    return (
+        <div>
+            test
+        </div>
+    )
+}
+
+export default Dashboard;
