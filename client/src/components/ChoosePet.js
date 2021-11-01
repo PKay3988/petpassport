871e0c2efe1549c7d80c582448a5366904410593
Tigana Sari <tiganasari@Portas-MB15.broadband>
2021-11-01 20:24:43 +0000

    ChosePet component

--- client/src/components/ChoosePet.js
+++ client/src/components/ChoosePet.js
@@ -3,7 +3,19 @@ import React from 'react'
 export const ChoosePet = () => {
     return (
         <div>
-            
+            <h1>Choose a pet</h1>
+
+            <ul>
+
+            <li> Butterscotch
+            </li>
+            <li> Mr Peanut
+            </li>
+            </ul>
+
+            <button>add a pet</button>
+            <button>delete a pet</button>
+
         </div>
     )
 }
