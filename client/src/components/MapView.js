4f26ed5392e120ab6b8200df7fb686f4f71efa49
Elisa Rotger <elisar021@gmail.com>
2021-11-04 23:23:59 +0100

    customisable markers

--- client/src/components/MapView.js
+++ client/src/components/MapView.js
@@ -61,12 +61,25 @@ const MapView = (props) => {
             : null}
 =======
 import React from "react";
+import L from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
-import Icon from '../assets/icon-1.svg';
 import './Vets.css';
 
+// leaflet/dist/images/marker-icon.png
+import icon from '../assets/icon-2.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
 const MapView = () => {
+    
+    let DefaultIcon = L.icon({
+        iconUrl: icon,
+        shadowUrl: iconShadow,
+        iconSize: [25, 40]
+    });
+
+    L.Marker.prototype.options.icon = DefaultIcon;
+
     return (
         <MapContainer  center={[ 51.53813, -0.22522 ]} zoom={13}>
             <TileLayer 
