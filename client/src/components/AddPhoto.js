e7606c62dbfc62f39773ede588633017df3fe8aa
Kayla Cormier <kaylacormier@Kaylas-MacBook-Pro.local>
2021-11-08 14:33:05 -0600

    prep to pull Elisa's

---
+++ client/src/components/AddPhoto.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const AddPhoto = () => {
+    return (
+        <div>
+            <h1>Choose a PhotoGallery</h1>
+
+            <ul>
+
+            <li> Butterscotch
+            </li>
+            <li> Mr Peanut
+            </li>
+            </ul>
+
+            <button>add a pet</button>
+            <button>delete a pet</button>
+
+        </div>
+    )
+}
+
+export default AddPhoto;
