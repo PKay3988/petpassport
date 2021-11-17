cf945ecbe06ef83b6233e12cd068274ef1dc36b1
Elisa Rotger <elisar021@gmail.com>
2021-11-17 19:14:02 +0100

    test

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Location of static assets
 app.use(express.static(path.join(__dirname, '/client/build')));
