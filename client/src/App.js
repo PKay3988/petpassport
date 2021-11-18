2d305c4d6f935237c7ee7053c797b54b2a7f5b5d
Elisa Rotger <elisar021@gmail.com>
2021-11-18 12:17:27 +0100

    node instead of nodemon

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
 import ReactRoutes from "./ReactRoutes";
-import Vets from "./components/Vets";
-import Nav2 from "./components/Nav2";
-import RegisterUser from "./components/RegisterUser";
 
-// import { BrowserRouter as Switch, Router, Route, Link, useHistory } from 'react-router-dom';
-// import { Switch } from 'react-router';
 
 function App(props) {
      const [pet, setPet] = useState(props.pet);
+     
   return (
        <div>
 
-{/* <p>Yo!</p> */}
 
      <Router> 
-          {/* <Nav2 pet={pet}/> */}
 
           <div className="App">
-               {/* <h1> Pet Passport </h1> */}
-                    {/* <Nav /> */}
-                    <ReactRoutes /> 
+               <ReactRoutes /> 
           </div>
         
      </Router> 
